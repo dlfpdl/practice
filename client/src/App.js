0e621667e2433ef303c957ff3ff4f2ca87493266
이레 <Ray@minhong-iui-MacBookAir.local>
2021-10-08 18:24:38 +0900

    frontend some work

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react';
+import Navbar from './components/layout/navbar';
+import Landing from './components/layout/landing';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
+import Alert from './components/layout/alert';
+import Dashboard from './components/dashboard/dashboard';
+import Privateroute from './components/routing/privateroute';
+
+// Redux
+import { Provider } from 'react-redux';
+import Store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loaduser } from './actions/auth';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    Store.dispatch(loaduser());
+  });
+
+  return (
+    <Provider store={Store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Privateroute exact path='/dashboard' component={Dashboard} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ client/src/components/routing/privateroute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import auth from '../../reducers/auth';
+
+const Privateroute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      !auth.isAuthenticated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
+
+Privateroute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(Privateroute);
